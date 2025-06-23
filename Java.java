1c97fb0476fde3c66ca9f5ec0cfbb804cba49f96
José Carlos <Josecarlossilba342@gmail.com>
2025-06-23 11:44:45 -0600

    Primer commit

---
+++ Java.java
@@ -0,0 +1,20 @@
+public class Java{
+    public static void main(String[] args) {
+
+        String nombre = "José";
+        int a= 5;
+        int b= 10;
+        int suma= a+b;
+
+        System.out.println("Hola " + nombre + "!");
+        System.out.println("La suma de " + a + "+" + b + " es: " + suma );
+
+
+
+
+
+
+
+
+    }
+}
