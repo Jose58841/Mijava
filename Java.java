9fe528d15fc89fe5fbd3ed92f63aca8fa2c864b0
José Carlos <Josecarlossilba342@gmail.com>
2025-06-23 12:25:09 -0600

    Tercer commit

--- Java.java
+++ Java.java
@@ -5,9 +5,11 @@ public class Java{
         int a= 10;
         int b= 10;
         int suma= a+b;
+        int mult= a*b;
 
         System.out.println("Hola " + nombre + "!");
         System.out.println("La suma de " + a + "+" + b + " es: " + suma );
+        System.out.println("La mult de " + a + "*" + b + " es: " + mult);
 
 
 
